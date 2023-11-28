b1947c9a2bf1098a9cec2b0bdddcd2c54b92192b
cwt <cwt@cwtdeMacBook-Pro.local>
2023-11-28 11:04:37 +0800

    add inorder traversal non recursion

---
+++ src/main/java/coder/alex/map/BinaryTree.java
@@ -0,0 +1,35 @@
+package coder.alex.map;
+
+import java.util.Stack;
+
+public class BinaryTree {
+
+  class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int val;
+  }
+
+  Stack<TreeNode> stack = new Stack<>();
+
+  public void inorderTraversalNonRecursion(TreeNode root) {
+    if(root != null) {
+      pushLeft(root);
+    }
+
+    while(!stack.isEmpty()) {
+      TreeNode cur = stack.pop();
+      if(cur.right != null) {
+        pushLeft(cur.right);
+      }
+    }
+  }
+  
+  public void pushLeft(TreeNode root) {
+    while(root.left != null) {
+      stack.push(root);
+      root = root.left;
+    }
+    stack.push(root);
+  }
+}
